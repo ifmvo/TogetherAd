8a9a69fa6fc29375865ae2ffad2e490614dee845
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-03-12 14:26:37 +0800

    (●ﾟωﾟ●)

--- gradlew.bat
+++ gradlew.bat
@@ -1,9 +1,4 @@
 @if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
