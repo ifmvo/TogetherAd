09105b0e36aedf8d47d2977a3ac48230e9fcc802
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-03-12 10:57:19 +0800

    更新科大讯飞sdk

--- app/src/main/java/com/matthewchen/togetherad/App.kt
+++ app/src/main/java/com/matthewchen/togetherad/App.kt
@@ -13,28 +13,29 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val baiduIdMap = mutableMapOf<String, String?>(
-            TogetherAdConst.AD_SPLASH to "xxxxx",
-            TogetherAdConst.AD_INTER to "xxxxx",
-            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
-            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
-        )
-        TogetherAd.initBaiduAd(applicationContext, "xxxxx", baiduIdMap)
+//        val baiduIdMap = mutableMapOf<String, String?>(
+//            TogetherAdConst.AD_SPLASH to "xxxxx",
+//            TogetherAdConst.AD_INTER to "xxxxx",
+//            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
+//            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
+//        )
+//        TogetherAd.initBaiduAd(applicationContext, "xxxxx", baiduIdMap)
+//
+//        val gdtIdMap = mutableMapOf<String, String?>(
+//            TogetherAdConst.AD_SPLASH to "xxxxx",
+//            TogetherAdConst.AD_INTER to "xxxxx",
+//            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
+//            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
+//        )
+//        TogetherAd.initGDTAd(applicationContext, "xxxxx", gdtIdMap)
+//
+//        val xunFeiIdMap = mutableMapOf<String, String?>(
+//            TogetherAdConst.AD_SPLASH to "xxxxx",
+//            TogetherAdConst.AD_INTER to "xxxxx",
+//            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
+//            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
+//        )
+//        TogetherAd.initXunFeiAd(applicationContext, xunFeiIdMap)
 
-        val gdtIdMap = mutableMapOf<String, String?>(
-            TogetherAdConst.AD_SPLASH to "xxxxx",
-            TogetherAdConst.AD_INTER to "xxxxx",
-            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
-            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
-        )
-        TogetherAd.initGDTAd(applicationContext, "xxxxx", gdtIdMap)
-
-        val xunFeiIdMap = mutableMapOf<String, String?>(
-            TogetherAdConst.AD_SPLASH to "xxxxx",
-            TogetherAdConst.AD_INTER to "xxxxx",
-            TogetherAdConst.AD_FLOW_INDEX to "xxxxx",
-            TogetherAdConst.AD_TIEPIAN_LIVE to "xxxxx"
-        )
-        TogetherAd.initXunFeiAd(applicationContext, xunFeiIdMap)
     }
 }
