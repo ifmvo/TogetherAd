71bbc8a72cb9953420541b0d657edc0b6fd0de93
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-04-02 23:15:02 +0800

    good night

---
+++ core/src/main/java/com/ifmvo/togetherad/core/TogetherAd.kt
@@ -0,0 +1,14 @@
+package com.ifmvo.togetherad.core
+
+/* 
+ * (●ﾟωﾟ●)
+ * 
+ * Created by Matthew Chen on 2020-04-02.
+ */
+object TogetherAd {
+
+
+//    var adMap = mutableMapOf<String, >()
+
+
+}
