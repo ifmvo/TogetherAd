cba283c4682ad4d83d01a765d97625c8993cade8
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-05-02 22:42:37 +0800

    nothing

--- demo/src/main/java/com/ifmvo/togetherad/demo/App.kt
+++ demo/src/main/java/com/ifmvo/togetherad/demo/App.kt
@@ -2,7 +2,6 @@ package com.ifmvo.togetherad.demo
 
 import android.app.Application
 import com.ifmvo.togetherad.core.TogetherAd
-import com.ifmvo.togetherad.gdt.TogetherAdGdt
 
 /* 
  * (●ﾟωﾟ●)
@@ -18,9 +17,9 @@ class App : Application() {
 //                TogetherAdAlias.AD_REWARD to "945152437"
 //        ), isDebug = BuildConfig.DEBUG)
 //
-        TogetherAdGdt.init(this, AdProviderType.GDT.type, "1107469470", mapOf(
-                TogetherAdAlias.AD_REWARD to "6051815092224372"
-        ))
+//        TogetherAdGdt.init(this, AdProviderType.GDT.type, "1107469470", mapOf(
+//                TogetherAdAlias.AD_REWARD to "6051815092224372"
+//        ))
 //
 //        TogetherAdBaidu.init(this, AdProviderType.BAIDU.type, "d39ba09f", mapOf(
 //                TogetherAdAlias.AD_REWARD to "7045323"
