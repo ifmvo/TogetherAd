2c38e39ebfb9b81e6cb59bc7c540164958629932
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-05-04 13:28:57 +0800

    hello

--- demo/src/main/java/com/ifmvo/togetherad/demo/App.kt
+++ demo/src/main/java/com/ifmvo/togetherad/demo/App.kt
@@ -1,7 +1,10 @@
 package com.ifmvo.togetherad.demo
 
 import android.app.Application
+import com.ifmvo.togetherad.baidu.TogetherAdBaidu
 import com.ifmvo.togetherad.core.TogetherAd
+import com.ifmvo.togetherad.csj.TogetherAdCsj
+import com.ifmvo.togetherad.gdt.TogetherAdGdt
 
 /* 
  * (●ﾟωﾟ●)
@@ -13,17 +16,17 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        TogetherAdCsj.init(this, AdProviderType.CSJ.type, "5062695", this.getString(R.string.app_name), mapOf(
-//                TogetherAdAlias.AD_REWARD to "945152437"
-//        ), isDebug = BuildConfig.DEBUG)
-//
-//        TogetherAdGdt.init(this, AdProviderType.GDT.type, "1107469470", mapOf(
-//                TogetherAdAlias.AD_REWARD to "6051815092224372"
-//        ))
-//
-//        TogetherAdBaidu.init(this, AdProviderType.BAIDU.type, "d39ba09f", mapOf(
-//                TogetherAdAlias.AD_REWARD to "7045323"
-//        ))
+        TogetherAdCsj.init(this, AdProviderType.CSJ.type, "5062695", this.getString(R.string.app_name), mapOf(
+                TogetherAdAlias.AD_REWARD to "945152437"
+        ), isDebug = BuildConfig.DEBUG)
+
+        TogetherAdGdt.init(this, AdProviderType.GDT.type, "1107469470", mapOf(
+                TogetherAdAlias.AD_REWARD to "6051815092224372"
+        ))
+
+        TogetherAdBaidu.init(this, AdProviderType.BAIDU.type, "d39ba09f", mapOf(
+                TogetherAdAlias.AD_REWARD to "7045323"
+        ))
 
         TogetherAd.setPublicProviderRadio(mapOf(
                 AdProviderType.GDT.type to 1,
