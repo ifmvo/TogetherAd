b23d31464806a8c4b3239207ef27d279d44f89bd
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-06-30 10:23:50 +0800

    使用普通的RecyclerViewAdapter示例原生自渲染广告

---
+++ demo/src/main/java/com/ifmvo/togetherad/demo/native_/NativeAdapter.kt
@@ -0,0 +1,81 @@
+package com.ifmvo.togetherad.demo.native_
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.ifmvo.togetherad.core.TogetherAd
+import com.ifmvo.togetherad.core.helper.AdHelperNative
+import com.ifmvo.togetherad.core.listener.NativeViewListener
+import com.ifmvo.togetherad.core.utils.ScreenUtil
+import com.ifmvo.togetherad.demo.R
+
+/**
+ * 一个普通的多类型列表的 Adapter
+ *
+ * Created by Matthew Chen on 2020/6/30.
+ */
+class NativeAdapter(list: List<Any>, context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        private const val ITEM_VIEW_TYPE_AD = 0
+        private const val ITEM_VIEW_TYPE_CONTENT = 1
+    }
+
+    private var mList: List<Any> = list
+    private var mContext: Context = context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == ITEM_VIEW_TYPE_CONTENT) {
+            ContentViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_native_content, parent, false))
+        } else {
+            AdViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_native_ad, parent, false))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            ITEM_VIEW_TYPE_CONTENT -> {
+                val contentViewHolder = holder as ContentViewHolder
+                val contentDataEntity = mList[position] as ContentDataEntity
+                contentViewHolder.imageView.layoutParams.height = ScreenUtil.getDisplayMetricsWidth(mContext) * 9 / 16
+                TogetherAd.mImageLoader?.loadImage(mContext, contentViewHolder.imageView, contentDataEntity.imgUrl)
+                contentViewHolder.textView.text = contentDataEntity.title
+            }
+            ITEM_VIEW_TYPE_AD -> {
+                val adViewHolder = holder as AdViewHolder
+                adViewHolder.adContainer.removeAllViews()
+                AdHelperNative.show(mList[position], adViewHolder.adContainer, NativeTemplateCommon(), object : NativeViewListener {
+                    override fun onAdExposed(providerType: String) {
+                        Toast.makeText(mContext, "原生广告曝光了：$providerType", Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onAdClicked(providerType: String) {
+                        Toast.makeText(mContext, "原生广告点击了：$providerType", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (mList[position] is ContentDataEntity) ITEM_VIEW_TYPE_CONTENT else ITEM_VIEW_TYPE_AD
+    }
+}
+
+class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var adContainer: ViewGroup = itemView.findViewById(R.id.adContainer)
+}
+
+class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var imageView: ImageView = itemView.findViewById(R.id.img)
+    var textView: TextView = itemView.findViewById(R.id.txt)
+}
--- demo/src/main/java/com/ifmvo/togetherad/demo/native_/NativeRecyclerViewActivity.kt
+++ demo/src/main/java/com/ifmvo/togetherad/demo/native_/NativeRecyclerViewActivity.kt
@@ -1,14 +1,20 @@
 package com.ifmvo.togetherad.demo.native_
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import com.ifmvo.togetherad.core.helper.AdHelperNative
+import com.ifmvo.togetherad.core.listener.NativeListener
 import com.ifmvo.togetherad.demo.R
+import com.ifmvo.togetherad.demo.TogetherAdAlias
+import kotlinx.android.synthetic.main.activity_native_recyclerview.*
+
 
 /**
- * 具体用法在 Fragment 中
+ * 原生自渲染在 RecyclerView 中的用法
  *
  * Created by Matthew Chen on 2020-04-20.
  */
@@ -22,8 +28,78 @@ class NativeRecyclerViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_common)
+        setContentView(R.layout.activity_native_recyclerview)
 
-//        addFragment(R.id.fgContainer, NativeRecyclerViewFragment())
+        val contentData = getContentData()
+
+        requestAd {
+
+            //使用 RecyclerView 展示合并后的数据
+            val allList = mergeContentAd(contentData, it)
+            rv.layoutManager = LinearLayoutManager(this@NativeRecyclerViewActivity)
+            rv.adapter = NativeAdapter(allList, this@NativeRecyclerViewActivity)
+        }
+    }
+
+    /**
+     * 把内容List和广告List合并
+     */
+    private fun mergeContentAd(contentList: List<ContentDataEntity>, adList: List<Any>): List<Any> {
+
+        var nextAdPosition = 0
+        var lastUseAdPosition = 0
+
+        val multiItemList = mutableListOf<Any>()
+        repeat(contentList.size) {
+            multiItemList.add(contentList[it])
+            if (adList.isNotEmpty() && nextAdPosition == it) {
+                if (lastUseAdPosition > adList.size - 1) {
+                    lastUseAdPosition = 0
+                }
+                multiItemList.add(adList[lastUseAdPosition])
+                lastUseAdPosition += 1
+                nextAdPosition += 5
+            }
+        }
+        return multiItemList
+    }
+
+    /**
+     * 模拟真实的内容数据List
+     */
+    private fun getContentData(): List<ContentDataEntity> {
+        val contentList = mutableListOf<ContentDataEntity>()
+        for (index in 1..15) {
+            val title = "正文内容序号：$index"
+            contentList.add(ContentDataEntity(title = title, imgUrl = "https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590128472&t=657ec840a5c6c658430135ea8b1d35f0"))
+        }
+        return contentList
+    }
+
+    /**
+     * 请求广告List
+     */
+    private fun requestAd(onResult: (adList: List<Any>) -> Unit) {
+        AdHelperNative.getList(this@NativeRecyclerViewActivity, TogetherAdAlias.AD_NATIVE_RECYCLERVIEW, maxCount = 3, listener = object : NativeListener {
+            override fun onAdStartRequest(providerType: String) {
+                //每个提供商请求之前都会回调
+            }
+
+            override fun onAdLoaded(providerType: String, adList: List<Any>) {
+                onResult(adList)
+            }
+
+            override fun onAdFailed(providerType: String, failedMsg: String?) {
+                //单个提供商请求失败
+            }
+
+            override fun onAdFailedAll() {
+                Toast.makeText(this@NativeRecyclerViewActivity, "所有平台都请求失败了", Toast.LENGTH_LONG).show()
+                //所有的提供商都失败
+                onResult(mutableListOf())
+            }
+        })
     }
 }
+
+class ContentDataEntity(val title: String, val imgUrl: String)
