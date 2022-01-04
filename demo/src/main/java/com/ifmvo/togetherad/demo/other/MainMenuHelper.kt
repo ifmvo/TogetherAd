f43783b8467826398625b573421ec04e7e963cd1
Matthew_Chen <Matthew_Chen_1994@163.com>
2022-01-04 22:17:49 +0800

    5.2.0

--- demo/src/main/java/com/ifmvo/togetherad/demo/other/MainMenuHelper.kt
+++ demo/src/main/java/com/ifmvo/togetherad/demo/other/MainMenuHelper.kt
@@ -30,7 +30,7 @@ object MainMenuHelper {
 
     private val menuMainList = arrayListOf(
             mapOf(
-                    "title" to "广告大全 5.1.9",
+                    "title" to "广告大全 5.2.0",
                     "desc" to "穿山甲4.2.0.3；优量汇4.441.1311；快手3.3.15；百度5.91"
             ),
             mapOf(
